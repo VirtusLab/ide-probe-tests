01da2a61b211aaad2a26bcda38d1ad36167b77d3
odisseus <odisseus@users.noreply.github.com>
2020-09-08 14:11:56 +0200

    Building intellij-pants-plugin from sources

--- ci/run_docker.sh
+++ ci/run_docker.sh
@@ -2,9 +2,6 @@
 
 set -x
 
-find . -maxdepth 1 -regex '.*/pants_.*\.zip' -delete
-PANTS_ZIP=$(ls | grep 'pants_.*\.zip' | head -1 | head -c -1)
-
 DOCKER_IMAGE=ideprobe-pants:local
 DOCKER_DIRECTORY=/tmp/ideprobe/output
 HOST_DIRECTORY=/tmp/ideprobe/output
@@ -13,6 +10,5 @@ mkdir -p "${HOST_DIRECTORY}"
 docker run  \
   --mount type=bind,source="${HOST_DIRECTORY}",target="${DOCKER_DIRECTORY}" \
   -e TEST_PATTERN="${TEST_PATTERN}" \
-  -v ${PWD}/$PANTS_ZIP:/tmp/pants.zip \
   "${DOCKER_IMAGE}" \
   "$@"
--- driver/src/main/scala/com/twitter/intellij/pants/PantsPluginExtension.scala
+++ driver/src/main/scala/com/twitter/intellij/pants/PantsPluginExtension.scala
@@ -1,11 +1,14 @@
 package com.twitter.intellij.pants
 
-import org.virtuslab.ideprobe.dependencies.Plugin
+import org.virtuslab.ideprobe.dependencies.{DependencyProvider, Plugin}
+import org.virtuslab.ideprobe.pants.PantsPluginBuilder
 import org.virtuslab.ideprobe.{IdeProbeFixture, ProbeDriver}
 import scala.language.implicitConversions
 
 trait PantsPluginExtension extends OpenProjectFixture { this: IdeProbeFixture =>
   val pantsProbePlugin: Plugin = Plugin.Bundled(s"ideprobe-pants-${BuildInfo.version}.zip")
+
+  DependencyProvider.registerBuilder(PantsPluginBuilder)
 
   registerFixtureTransformer(_.withPlugin(pantsProbePlugin))
   registerFixtureTransformer(_.withAfterWorkspaceSetup(PantsSetup.overridePantsVersion))
