6f2a1bdc84a766245a4a042ddd2f135337e139dc
odisseus <odisseus@users.noreply.github.com>
2020-09-08 09:05:15 +0200

    setup-ci-environment.sh

--- driver/src/main/scala/org/virtuslab/ideprobe/pants/PantsPluginBuilder.scala
+++ driver/src/main/scala/org/virtuslab/ideprobe/pants/PantsPluginBuilder.scala
@@ -25,6 +25,10 @@ object PantsPluginBuilder extends DependencyBuilder(Id("pants")) {
   private def build(repository: Git, resources: ResourceProvider): InputStream = {
     val localRepo = GitUtils.clone(repository)
 
+    val command0 = List("./scripts/setup-ci-environment.sh")
+    val result0 = Shell.run(localRepo, Map.empty[String, String], command0: _*)
+    if (result0.exitCode != 0) throw new Exception(s"Couldn't set up ci environment. STDERR:\n${result0.err}")
+
     val env = Map("TRAVIS_BRANCH" -> "master")
     val command = List("./scripts/deploy/deploy.sh", "--skip-publish")
     val result = Shell.run(localRepo, env, command: _*)
