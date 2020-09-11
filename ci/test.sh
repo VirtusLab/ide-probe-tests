ced68bbfafe6aca785fc6a7b8241eae163bf8d87
odisseus <odisseus@users.noreply.github.com>
2020-09-11 16:45:54 +0200

    same container

--- ci/test.sh
+++ ci/test.sh
@@ -4,6 +4,8 @@ set -x
 export IDEPROBE_DISPLAY=xvfb
 
 apt install -y zip
+sbt "ciSetup/testOnly com.twitter.intellij.pants.PreparePants"
+echo "ALL PANTS READY!"
 if [ -z "${TEST_PATTERN}" ]; then
   sbt "pantsTests/test"
 else
