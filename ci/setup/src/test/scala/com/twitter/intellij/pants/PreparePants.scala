70831cb5a61554e746a6c7d104208f17417ea75a
odisseus <odisseus@users.noreply.github.com>
2020-09-14 16:12:38 +0200

    Use an updated image to speed up the tests

--- ci/setup/src/test/scala/com/twitter/intellij/pants/PreparePants.scala
+++ ci/setup/src/test/scala/com/twitter/intellij/pants/PreparePants.scala
@@ -1,18 +1,24 @@
 package com.twitter.intellij.pants
 
+import java.net.URI
+
 import org.junit.Test
-import org.virtuslab.ideprobe.Config
+import org.virtuslab.ideprobe.{Config, Id, IntelliJFixture}
 import org.virtuslab.ideprobe.Extensions._
-import org.virtuslab.ideprobe.IntelliJFixture
+import org.virtuslab.ideprobe.dependencies.Plugin
+import org.virtuslab.ideprobe.dependencies.SourceRepository.Git
 
 class PreparePants extends PantsTestSuite {
   @Test def run(): Unit = {
     val fixture = IntelliJFixture.fromConfig(Config.fromClasspath("pants.conf"))
+      .withPlugin(Plugin.FromSources(Id("pants"), Git(new URI("https://github.com/pantsbuild/intellij-pants-plugin"), None)))
     val workspace = fixture.setupWorkspace()
+    val intellij = fixture.installIntelliJ()
     workspace.resolve("pants").write("dummy").makeExecutable()
     PantsSetup.overridePantsVersion(fixture, workspace)
     runPants(workspace, Seq("goals"))
     runFastpass(fixture.config, workspace, Seq("--version"))
+    fixture.deleteIntelliJ(intellij)
     fixture.deleteWorkspace(workspace)
   }
 }
