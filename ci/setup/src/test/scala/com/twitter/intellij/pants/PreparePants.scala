b7241e4e1cd52f569ac5146e5e449513b5452af3
odisseus <odisseus@users.noreply.github.com>
2021-03-08 12:57:38 +0100

    Use the binary distribution of the Pants plugin instead of building it from sources

--- ci/setup/src/test/scala/com/twitter/intellij/pants/PreparePants.scala
+++ ci/setup/src/test/scala/com/twitter/intellij/pants/PreparePants.scala
@@ -1,38 +1,25 @@
 package com.twitter.intellij.pants
 
 import com.typesafe.config.ConfigObject
-import java.net.URI
 import org.junit.Test
-import org.virtuslab.ideprobe.{Config, Id, IntelliJFixture}
+import org.virtuslab.ideprobe.{Config, IntelliJFixture}
 import org.virtuslab.ideprobe.Extensions._
-import org.virtuslab.ideprobe.dependencies.{GitRepository, Plugin}
+import org.virtuslab.ideprobe.dependencies.Plugin
 import org.virtuslab.ideprobe.pants.PantsSetup
 import org.virtuslab.tests.pants.PantsTestSuite
-import pureconfig.{ConfigSource, ConfigWriter}
 
 class PreparePants extends PantsTestSuite {
   @Test def run(): Unit = {
-    val pantsPluginRepository = "https://github.com/pantsbuild/intellij-pants-plugin"
-    val repository = GitRepository(new URI(pantsPluginRepository), ref = None)
-    val config = toBuildConfig(repository)
     val fixture = IntelliJFixture
       .fromConfig(Config.fromClasspath("pants.conf"))
-      .withPlugin(Plugin.FromSources(Id("pants"), config))
+      .withPlugin(Plugin.Versioned("com.intellij.plugins.pants", version = "1.17.0.2ee673770d628ec4c49cddf49a15bc8c6ab2457c", channel = Some("bleedingedge")))
     val workspace = fixture.setupWorkspace()
     val intellij = fixture.installIntelliJ()
     workspace.resolve("pants").write("dummy").makeExecutable()
     PantsSetup.overridePantsVersion(fixture, workspace)
-    runPants(workspace, Seq("goals"))
+    runPants(workspace, Seq("help", "goals"))
     runFastpass(fixture.config, workspace, Seq("--version"))
     fixture.deleteIntelliJ(intellij)
     fixture.deleteWorkspace(workspace)
-  }
-
-  private def toBuildConfig(repository: GitRepository) = {
-    val config = ConfigWriter[Map[String, GitRepository]]
-      .to(Map("repository" -> repository))
-      .asInstanceOf[ConfigObject]
-      .toConfig
-    Config(ConfigSource.fromConfig(config))
   }
 }
