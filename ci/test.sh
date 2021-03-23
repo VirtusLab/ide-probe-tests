7e6985665807101e7f612f8962f76c0774adca36
odisseus <odisseus@users.noreply.github.com>
2021-03-23 08:31:47 +0100

    Use Coursier v2.0.13 in CI

--- ci/test.sh
+++ ci/test.sh
@@ -2,6 +2,7 @@
 set -x
 
 export IDEPROBE_DISPLAY=xvfb
+export FASTPASS_COURSIER_URL=https://github.com/coursier/coursier/releases/download/v2.0.13/coursier
 
 if [ -z "${TEST_PATTERN}" ]; then
   sbt "$TEST_TARGET/test"
