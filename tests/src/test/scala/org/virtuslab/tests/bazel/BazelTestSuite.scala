76180f9be9662e1a6738b8a2a2426321d58f6a4f
Lukasz Wawrzyk <uwawrzyk@twitter.com>
2021-07-27 10:59:40 +0200

    resolver

--- tests/src/test/scala/org/virtuslab/tests/bazel/BazelTestSuite.scala
+++ tests/src/test/scala/org/virtuslab/tests/bazel/BazelTestSuite.scala
@@ -1,6 +1,23 @@
 package org.virtuslab.tests.bazel
 
+import java.net.URI
+import org.virtuslab.ideprobe.Extensions.URIExtension
 import org.virtuslab.ideprobe.bazel.BazelPluginExtension
+import org.virtuslab.ideprobe.dependencies.{Dependency, DependencyProvider, IntelliJDependencyProvider, IntelliJVersion, IntelliJZipResolver, PluginDependencyProvider, PluginResolver, ResourceProvider}
+import org.virtuslab.ideprobe.ide.intellij.IntelliJFactory
 import org.virtuslab.ideprobe.junit4.IdeProbeTestSuite
 
-trait BazelTestSuite extends IdeProbeTestSuite with BazelPluginExtension
+trait BazelTestSuite extends IdeProbeTestSuite with BazelPluginExtension {
+
+  registerFixtureTransformer(f => {
+    val dependencies = new DependencyProvider(
+      new IntelliJDependencyProvider(Seq({
+        version: IntelliJVersion =>
+          val name = s"ideaIC-${version.release.getOrElse(version.build)}.portable.zip"
+          Dependency.Artifact(URI.create("https://download.jetbrains.com/idea").resolveChild(name))
+      },IntelliJZipResolver.Community), ResourceProvider.Default),
+      new PluginDependencyProvider(Seq(PluginResolver.Official), ResourceProvider.Default)
+    )
+    f.copy(intelliJProvider = IntelliJFactory(dependencies, f.intelliJProvider.plugins, f.intelliJProvider.version, f.intelliJProvider.paths, f.intelliJProvider.config))
+  })
+}
--- tests/src/test/scala/org/virtuslab/tests/pants/PantsTestSuite.scala
+++ tests/src/test/scala/org/virtuslab/tests/pants/PantsTestSuite.scala
@@ -1,9 +1,27 @@
 package org.virtuslab.tests.pants
 
+import java.net.URI
+import org.virtuslab.ideprobe.Extensions.URIExtension
+import org.virtuslab.ideprobe.dependencies.{Dependency, DependencyProvider, IntelliJDependencyProvider, IntelliJVersion, IntelliJZipResolver, PluginDependencyProvider, PluginResolver, ResourceProvider}
+import org.virtuslab.ideprobe.ide.intellij.IntelliJFactory
 import org.virtuslab.ideprobe.junit4.IdeProbeTestSuite
 import org.virtuslab.ideprobe.pants.{PantsPluginExtension, PantsPluginExtraExtensions}
 
 trait PantsTestSuite
   extends IdeProbeTestSuite
     with PantsPluginExtension
-    with PantsPluginExtraExtensions
+    with PantsPluginExtraExtensions {
+
+
+  registerFixtureTransformer(f => {
+    val dependencies = new DependencyProvider(
+      new IntelliJDependencyProvider(Seq({
+        version: IntelliJVersion =>
+          val name = s"ideaIC-${version.release.getOrElse(version.build)}.portable.zip"
+          Dependency.Artifact(URI.create("https://download.jetbrains.com/idea").resolveChild(name))
+      },IntelliJZipResolver.Community), ResourceProvider.Default),
+      new PluginDependencyProvider(Seq(PluginResolver.Official), ResourceProvider.Default)
+    )
+    f.copy(intelliJProvider = IntelliJFactory(dependencies, f.intelliJProvider.plugins, f.intelliJProvider.version, f.intelliJProvider.paths, f.intelliJProvider.config))
+  })
+}
