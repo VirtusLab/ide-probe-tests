3028bdbd4c2e3ca67789b3168ce4d2a398d84198
Lukasz Wawrzyk <uwawrzyk@twitter.com>
2021-08-17 13:06:35 +0200

    pool test

--- tests/src/main/scala/org/virtuslab/tests/pants/BUILDFilesTest.scala
+++ tests/src/main/scala/org/virtuslab/tests/pants/BUILDFilesTest.scala
@@ -1,20 +1,24 @@
 package org.virtuslab.tests.pants
 
-import java.util.concurrent.Executors
+import java.util.concurrent.{ExecutorService, Executors, ForkJoinPool, SynchronousQueue, ThreadPoolExecutor, TimeUnit}
 import org.virtuslab.ideprobe.{Assertions, IdeProbeFixture, Shell}
 import org.virtuslab.ideprobe.robot.RobotPluginExtension
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 object BUILDFilesTest extends IdeProbeFixture
   with RobotPluginExtension with Assertions {
+
+  val pool = new ThreadPoolExecutor(0, Integer
+    .MAX_VALUE, 5L, TimeUnit.SECONDS, new SynchronousQueue[Runnable])
+  pool.allowCoreThreadTimeOut(true)
+  override protected implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(pool)
 
   def main(args: Array[String]): Unit = {
     fixtureFromConfig().run { intelliJ =>
       intelliJ.probe.withRobot.openProject(intelliJ.workspace)
       intelliJ.probe.projectModel()
     }
-
-    Future(Shell.run("jstack", ProcessHandle.current.pid.toString))(scala.concurrent.ExecutionContext.global)
-}
+//    ec.execute(() => println("sad"))
+  }
 
 }
