6e0e6e6360e54e5fdac1d8a8748792c2ff2d9d53
Mikolaj <mkoziel045@gmail.com>
2021-09-21 12:35:05 +0200

    turn off robot

--- tests/src/test/scala/org/virtuslab/tests/pants/PantsTestSuite.scala
+++ tests/src/test/scala/org/virtuslab/tests/pants/PantsTestSuite.scala
@@ -1,9 +1,21 @@
 package org.virtuslab.tests.pants
 
+import org.virtuslab.ideprobe.{RunningIntelliJFixture, WaitLogic}
 import org.virtuslab.ideprobe.junit4.IdeProbeTestSuite
 import org.virtuslab.ideprobe.pants.{PantsPluginExtension, PantsPluginExtraExtensions}
+import org.virtuslab.ideprobe.protocol.ProjectRef
 
 trait PantsTestSuite
   extends IdeProbeTestSuite
     with PantsPluginExtension
     with PantsPluginExtraExtensions
+{
+  override def openProjectWithPants(intelliJ: RunningIntelliJFixture, waitLogic: WaitLogic): ProjectRef = {
+    val projectPath = runPantsIdeaPlugin(intelliJ.workspace, targetsFromConfig(intelliJ))
+    intelliJ.probe.openProject(projectPath, waitLogic)
+  }
+
+  private def targetsFromConfig(intelliJ: RunningIntelliJFixture): Seq[String] = {
+    intelliJ.config[Seq[String]]("pants.import.targets")
+  }
+}
