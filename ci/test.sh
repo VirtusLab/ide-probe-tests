70997bc74790e799b2ed255d691d06ec637fddcc
odisseus <odisseus@users.noreply.github.com>
2023-02-16 01:46:55 +0100

    Bump Coursier to v2.1.0-RC5

--- ci/test.sh
+++ ci/test.sh
@@ -2,7 +2,7 @@
 set -x
 
 export IDEPROBE_DISPLAY=xvfb
-export FASTPASS_COURSIER_URL=https://github.com/coursier/coursier/releases/download/v2.0.14/coursier
+export FASTPASS_COURSIER_URL=https://github.com/coursier/coursier/releases/download/v2.1.0-RC5/coursier
 
 if [ -z "${TEST_PATTERN}" ]; then
   sbt "$TEST_TARGET/test"
